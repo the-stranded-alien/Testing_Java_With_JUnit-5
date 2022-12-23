c71c80ac47bf73d2ba34afa041e0af63a02cbcd2
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-23 12:46:24 +0530

    Tags for JUnit

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,11 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@Tag("model")
 class OwnerTest {
 
     @Test
