dd84ee53cad08c6150143ae0c5d4acefe6617a41
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-23 13:35:27 +0530

    Repeating Tests

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.ModelTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -28,4 +30,10 @@ class PersonTest implements ModelTests {
                 () -> assertEquals("Buck", person.getLastName(), "Last Name Failed"));
     }
 
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+    @DisplayName("My Repeated Test")
+    @Test
+    void myRepeatedTest() {
+        // todo - impl
+    }
 }
